6b025abec038f52d0f8e245879bb4b5a84ad039c
Matheus <matheusdebrito81@gmail.com>
2024-01-24 19:49:47 -0300

    criados componentes: title, sobre, projetos e contatos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleComponent } from './components/title/title.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ProjetosComponent } from './components/projetos/projetos.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    SobreComponent,
+    ProjetosComponent,
+    ContatosComponent
   ],
   imports: [
     BrowserModule,
