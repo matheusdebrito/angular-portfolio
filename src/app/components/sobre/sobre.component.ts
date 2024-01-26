c816d781428890e90b644820d406acdb26a56d0c
Matheus <matheusdebrito81@gmail.com>
2024-01-25 23:14:45 -0300

    Adicionadas funções para navegar pelo menu e criado menu hamburger para telas pequenas

--- src/app/components/sobre/sobre.component.ts
+++ src/app/components/sobre/sobre.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './sobre.component.css'
 })
 export class SobreComponent {
-
+  
 }
--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
+import { query } from '@angular/animations';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrl: './title.component.css'
+  styleUrls: ['./title.component.css']
 })
-export class TitleComponent {
-  
+export class TitleComponent implements OnInit{
 
-  constructor(){
+  appRoot: any = document.querySelector("app-root")
 
+  moveToHome(){
+    this.appRoot?.querySelector("app-sobre .container").scrollIntoView({behavior: 'smooth', block: 'center'})
+    let divMenu = this.appRoot.querySelector(".menu")
+    divMenu.classList.toggle("active")
   }
 
+  moveToProjetos(){
+    this.appRoot?.querySelector("app-projetos .container").scrollIntoView({behavior: 'smooth', block: 'center'})
+    let divMenu = this.appRoot.querySelector(".menu")
+    divMenu.classList.toggle("active")
+  }
+
+  moveToContatos(){
+    this.appRoot?.querySelector("app-contatos .container").scrollIntoView({behavior: 'smooth', block: 'end'})
+    let divMenu = this.appRoot.querySelector(".menu")
+    divMenu.classList.toggle("active")
+  }
+
+  btnMobile(){
+    let divMenu = this.appRoot.querySelector(".menu")
+    divMenu.classList.toggle("active")
+  }
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
 
 }
